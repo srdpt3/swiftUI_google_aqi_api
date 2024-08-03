a72f76b03b1087d57316dfa5179efdbaffb1661b
Dustin Yang <dustinyang@DustinucBookPro.lan>
2024-08-02 20:56:52 -0400

    update

--- AppViewModel.swift
+++ AppViewModel.swift
@@ -10,16 +10,110 @@ import MapKit
 import Observation
 import SwiftUI
 import XCAAQI
+import Observation
+//struct Location : Identifiable
+//{
+//    var id = UUID()
+//    var coordinate : CLLocationCoordinate2D
+//    var aqIndex: String
+//}
+enum LocationStatus: Equatable {
+case requestingLocation
+case locationNotAutorized(String)
+case error(String)
+case requestingAQIConditons
+case standby
+
+}
+
 
 @Observable
-class AppViewModel {
-    let aqiClient = AirQualityClient(apiKey: "AIzaSyBRFywqkh_-QZJAyv1nwtCruRWX3-lyOCE")
+class AppViewModel: NSObject {
+    let locationManager = CLLocationManager()
+    let aqiClient = AirQualityClient(apiKey: "GOOGLE_KEY")
+    let coordinatesFinder = CoordinatesFinder()
+
     var currentLocation : CLLocationCoordinate2D?
+    var locationStatus = LocationStatus.requestingLocation
     var position : MapCameraPosition = .automatic
-    var annotaiton : [(CLLocationCoordinate2D, Int)] = []
-    
-    init(){
-        self.currentLocation = .init(latitude: 40.776676, longitude: -73.971321)
-        self.position = .region(.init(center: currentLocation!, latitudinalMeters: 0, longitudinalMeters: 16000))
+    var annotaiton : [AQIResponse] = []
+    var selection : AQIResponse?
+    var presentationDetent = PresentationDetent.height(176)
+    var lat : Double = 0
+    var lon : Double = 0
+
+    var radiusArray: [(Double, Int)]
+
+    init(radiusArray: [(Double, Int)] = [(4000,1), (8000, 1)]){
+        self.radiusArray = radiusArray
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        
+//        self.currentLocation = .init(latitude: 40.776676, longitude: -73.971321)
+//        Task{
+//            await self.handleCoordinateChange(currentLocation!)
+//        }
+
+}
+
+    @MainActor
+func handleCoordinateChange(_ coordinate: CLLocationCoordinate2D) async {
+    do{
+        self.locationStatus = .requestingAQIConditons
+        self.position = .region(.init(center: coordinate, latitudinalMeters: 0, longitudinalMeters: 16000))
+        let coordinates = getCoordinatesAround(coordinate)
+        self.annotaiton = try await aqiClient.getCurrentConditions(coordinates: coordinates.map{
+            ($0.latitude, $0.longitude)
+        })
+        
+        self.locationStatus = .standby
+     
+    } catch{
+        self.locationStatus = .error(error.localizedDescription)
     }
+    
+  
+//        self.annotaiton = getCoordinatesAround(coordinate).map{
+//            Location(coordinate: $0, aqIndex: "\((50...150).randomElement()!)")
+//        }
+
+}
+
+func getCoordinatesAround(_ coordinate: CLLocationCoordinate2D) -> [CLLocationCoordinate2D]{
+    var results : [CLLocationCoordinate2D] = [coordinate]
+    radiusArray.forEach {
+        results += coordinatesFinder.findCoordinates(coordinate, r: $0.0, n: $0.1)
+    }
+    
+    return results
+}
+}
+
+extension AppViewModel: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            manager.requestLocation()
+        default:
+            self.locationStatus = .locationNotAutorized("Unauthorized location access")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.locationStatus = .error(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coordinate = locations.first?.coordinate else { return }
+        if currentLocation == nil {
+            lat = coordinate.latitude
+            lon = coordinate.longitude
+            Task { await self.handleCoordinateChange(coordinate)}
+        }
+        currentLocation = coordinate
+    }
+    
 }
--- ContentView.swift
+++ ContentView.swift
@@ -7,24 +7,129 @@
 
 import SwiftUI
 import MapKit
+import XCAAQI
 
 struct ContentView: View {
     
     @State var vm = AppViewModel()
     
     var body: some View {
-        Map(position: $vm.position){
-            Marker("내위치", coordinate: vm.currentLocation!).annotationTitles(.hidden)
-                
+        Map(position: $vm.position, selection: $vm.selection){
+//            Marker("내위치", coordinate: vm.currentLocation!).annotationTitles(.hidden)
+            ForEach(vm.annotaiton){ aqi in
+                Annotation(aqi.aqiDisplay, coordinate: aqi.coordinate) {
+                    CircleAQIView(aqi: aqi, isSelected: aqi == vm.selection)
+                }.annotationTitles(.hidden)
+                    .tag(aqi)
+            }
+                3
             
         }.mapStyle(.hybrid(elevation: .flat, pointsOfInterest: .all, showsTraffic: false))
             .mapControls {
                 MapUserLocationButton()
                 MapCompass()
             }
+            .sheet(isPresented: .constant(true)) {
+                ScrollView{
+                    VStack{
+                        if let selection = vm.selection{
+                            selectedAQIView(aqi: selection)
+                        }else{
+                            if vm.locationStatus != .requestingLocation && vm.locationStatus != .requestingAQIConditons {
+                                locationFormview
+                            }
+                            if vm.locationStatus == .requestingAQIConditons{
+                                ProgressView("Requesting Current Air Quality Conditions....")
+                            }
+                            
+                            if vm.locationStatus == .requestingLocation{
+                                ProgressView("Requesting Current Location")
+                            }
+                            
+                            if case let .locationNotAutorized(text) = vm.locationStatus {
+                                Text(text)
+                            }
+                            
+                            if case let .error(text) = vm.locationStatus {
+                                Text(text)
+                            }
+                            
+                        }
+                    }
+                    
+                }
+                .padding()
+                .safeAreaPadding(.top)
+                .presentationDetents([.height(24), .height(176)], selection: $vm.presentationDetent)
+                .presentationBackground(.ultraThinMaterial)
+                .presentationBackgroundInteraction(.enabled(upThrough: .height(176)))
+                .interactiveDismissDisabled()
+            }
+            .onChange(of: vm.selection, { oldValue, newValue in
+                if oldValue == nil && newValue != nil {
+                    vm.presentationDetent = .height(176)
+                }
+            })
+            .navigationTitle("Air Quality Index")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.ultraThinMaterial, for: .navigationBar)
+    }
+    
+    
+    func selectedAQIView(aqi: AQIResponse) -> some View {
+        HStack(spacing: 16){
+            CircleAQIView(aqi: aqi, size: CGSize(width: 80, height: 80))
+                .padding(.leading)
+            VStack(alignment: .leading) {
+                Text("Coordinate: \(aqi.coordinate.latitude), \(aqi.coordinate.longitude)")
+                Text(aqi.category)
+                Text("Dominant Pollutant : \(aqi.dominantPollutant)")
+                Text(aqi.displayName)
+
+            }.padding(.top)
+             .padding(.horizontal)
+
+             .frame(maxWidth : .infinity)
+        }
+
+    }
+    @ViewBuilder
+    var locationFormview: some View {
+        Text("Get Current SQI aorund a coordinate").font(.headline).padding(.bottom, 8)
+        
+         HStack {
+             Text("Lat")
+             TextField("Enter Latitude", value: $vm.lat, format: .number)
+             Text("Lon")
+             TextField("Enter Longitude", value: $vm.lon, format: .number)
+         }
+         .keyboardType(.decimalPad)
+         .textFieldStyle(.roundedBorder)
+         .padding(.bottom, 8)
+        
+        HStack{
+            Button("Use Current Loc"){
+                vm.lat = vm.currentLocation?.latitude ?? 0
+                vm.lon = vm.currentLocation?.longitude ?? 0
+                Task {
+                    await vm.handleCoordinateChange(.init(latitude: vm.lat, longitude: vm.lon))
+                }
+            }.buttonStyle(.borderedProminent)
+            
+            Button("Refresh Data"){
+            
+                Task {
+                    await vm.handleCoordinateChange(.init(latitude: vm.lat, longitude: vm.lon))
+                }
+            }.buttonStyle(.borderedProminent)
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack{
+        ContentView(vm: .init(radiusArray: [(4000, 1), (8000,1)]))
+
+    }
+//    , 126.89543423863009
 }
